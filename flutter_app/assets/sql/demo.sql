767a2e6aa4787db2ce7806656525a3d854105179
kvnvodev <kvnvo.dev@gmail.com>
2019-03-28 23:02:25 +0700

    Update demo sql

--- flutter_app/assets/sql/demo.sql
+++ flutter_app/assets/sql/demo.sql
@@ -33,4 +33,28 @@ CREATE TABLE IF NOT EXISTS order_details (
     FOREIGN KEY (order_id) REFERENCES orders (id)
 );
 
+INSERT INTO categories (name) VALUES ("Foods & Beverages"), ("Drinks");
+
+INSERT INTO products (name, price, category_id) VALUES 
+("Expresso", 30000, 2), 
+("Latte", 34000, 2), 
+("Cappuccino", 37000, 2), 
+("Crossaints", 19000, 1), 
+("Donuts", 16000, 1),
+("Sandwich", 23000, 1),
+("Hamburger", 34000, 1),
+("Mashed potato", 23000, 1);
+
 INSERT INTO order_status (name) VALUES ("New"), ("In Progress"), ("Canceled"), ("Done");
+
+INSERT INTO orders (status_id) VALUES (1), (1), (1), (1);
+
+INSERT INTO order_details (product_id, order_id, price, quantity) VALUES
+(1, 1, 30000, 1),
+(2, 1, 34000, 2),
+(2, 2, 34000, 1),
+(4, 2, 19000, 2),
+(8, 2, 23000, 1),
+(5, 3, 19000, 3),
+(7, 3, 23000, 2),
+(1, 3, 23000, 5);
