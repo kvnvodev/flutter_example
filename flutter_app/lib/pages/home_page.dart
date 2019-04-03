3b5e72680cc1ca5f3b42f8d7ab5ed446b868be3b
Kevin Vo <kvnvo.dev@gmail.com>
2019-04-03 22:21:59 +0700

    add animation

--- flutter_app/lib/pages/home_page.dart
+++ flutter_app/lib/pages/home_page.dart
@@ -97,16 +97,48 @@ class Backdrop extends StatefulWidget {
   _BackdropState createState() => _BackdropState();
 }
 
-class _BackdropState extends State<Backdrop> {
+class _BackdropState extends State<Backdrop> with TickerProviderStateMixin {
+  AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 200));
+  }
+
   Widget _appBar() {
     return AppBar(
-      // leading: AnimatedIcon(),
+      leading: IconButton(
+          icon: AnimatedIcon(
+              icon: AnimatedIcons.menu_close, progress: animationController),
+          onPressed: () {
+            if (animationController.status == AnimationStatus.dismissed ||
+                animationController.status == AnimationStatus.forward) {
+              // animationController.forward();
+              animationController.fling(velocity: 3.0);
+            } else {
+              animationController.fling(velocity: -3.0);
+              // animationController.reverse();
+            }
+            // setState(() {});
+          }),
       title: Text("Demo App"),
     );
   }
 
   Widget _body() {
     return LayoutBuilder(builder: (context, constraints) {
+      const double layerTitleHeight = 50.0;
+      final Size layerSize = constraints.biggest;
+      final double layerTop = layerSize.height - layerTitleHeight;
+
+      Animation<RelativeRect> layerAnimation = RelativeRectTween(
+        begin: RelativeRect.fromLTRB(0.0, 0.0, 0.0, 0.0),
+        end: RelativeRect.fromLTRB(
+            0.0, layerTop, 0.0, layerTop - layerSize.height),
+      ).animate(animationController.view);
+
       return Stack(
         children: <Widget>[
           Container(
@@ -114,21 +146,24 @@ class _BackdropState extends State<Backdrop> {
               height: constraints.biggest.height,
               width: constraints.biggest.width,
               child: widget.backLayer),
-          Material(
-              color: Colors.white,
-              elevation: 16.0,
-              shape: BeveledRectangleBorder(
-                  borderRadius:
-                      BorderRadius.only(topLeft: Radius.circular(50.0))),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  Container(height: 50),
-                  Expanded(
-                      child: Container(
-                          color: Colors.white, child: widget.frontLayer))
-                ],
-              ))
+          PositionedTransition(
+            rect: layerAnimation,
+            child: Material(
+                color: Colors.white,
+                elevation: 16.0,
+                shape: BeveledRectangleBorder(
+                    borderRadius:
+                        BorderRadius.only(topLeft: Radius.circular(50.0))),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Container(height: 50),
+                    Expanded(
+                        child: Container(
+                            color: Colors.white, child: widget.frontLayer))
+                  ],
+                )),
+          ),
         ],
       );
     });
