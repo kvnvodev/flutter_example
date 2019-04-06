2d78e07abd0d56ff63e6e66c2c9016b984593085
Kevin Vo <kvnvo.dev@gmail.com>
2019-04-06 10:10:55 +0700

    Update main.

--- planning_poker/lib/main.dart
+++ planning_poker/lib/main.dart
@@ -1,111 +1,423 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        title: 'Flutter Demo',
+        // theme: ThemeData(
+        //     textTheme: TextTheme().copyWith(button: TextStyle(color: Colors.cyan)),
+        //     buttonTheme: ButtonThemeData().copyWith(
+        //         buttonColor: Colors.amber),
+        //     inputDecorationTheme:
+        //         InputDecorationTheme(border: OutlineInputBorder())),
+        home: P2HomePage(),
+        initialRoute: "/login",
+        onGenerateRoute: (RouteSettings settings) {
+          if (settings.name != "/login") {
+            return null;
+          }
+
+          return MaterialPageRoute(
+              fullscreenDialog: true,
+              settings: settings,
+              builder: (BuildContext context) => P2LoginPage());
+        });
+  }
+}
+
+class P2LoginPage extends StatefulWidget {
+  @override
+  _P2LoginPageState createState() => _P2LoginPageState();
+}
+
+class _P2LoginPageState extends State<P2LoginPage> {
+  TextEditingController _usernameController;
+  TextEditingController _passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _usernameController = TextEditingController();
+    _passwordController = TextEditingController();
+  }
+
+  void _cancelLogin() {
+    Navigator.of(context).pop();
+  }
+
+  void _login() {
+    if (_usernameController.text == "admin" &&
+        _passwordController.text == "admin") {}
+
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        // padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          children: [
+            Container(height: 200.0, color: Theme.of(context).primaryColor),
+            const SizedBox(height: 16.0),
+            TextField(
+                controller: _usernameController,
+                decoration: InputDecoration(
+                    labelText: "Username", hintText: "Username")),
+            const SizedBox(height: 16.0),
+            TextField(
+                controller: _passwordController,
+                decoration: InputDecoration(
+                    labelText: "Password", hintText: "Password"),
+                obscureText: true),
+            const SizedBox(height: 16.0),
+            ButtonBar(children: [
+              FlatButton(onPressed: _cancelLogin, child: Text("Cancel")),
+              RaisedButton(
+                onPressed: _login,
+                child: Text("Login"),
+              )
+            ]),
+            FlatButton(onPressed: () {}, child: Text("Forgot password"))
+          ],
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class P2HomePage extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _P2HomePageState createState() => _P2HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _P2HomePageState extends State<P2HomePage> with TickerProviderStateMixin {
+  int val = 8;
+  Axis direction = Axis.vertical;
+  double fraction = 0.5;
+  PageController pageController;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    direction = Axis.vertical;
+    fraction = 0.5;
+    pageController = PageController(viewportFraction: fraction);
+    animationController = AnimationController(
+        vsync: this, duration: Duration(milliseconds: 3000));
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    int count = 3;
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      double width = constraints.biggest.width;
+      double height = constraints.biggest.height / count;
+
+      List<Widget> list = List<Widget>.generate(count, (int index) {
+        double top = index * height;
+        Animation<RelativeRect> anim = animationController
+            .drive(CurveTween(curve: Curves.easeIn))
+            .drive(RelativeRectTween(
+                begin: RelativeRect.fromLTRB(0, 0, 0, 0),
+                end: RelativeRect.fromLTRB(0, -top, 0, top)));
+
+        return Expanded(
+            child: Stack(overflow: Overflow.visible, children: <Widget>[
+          PositionedTransition(
+              rect: anim,
+              child: Container(
+                  width: width,
+                  height: height,
+                  color: [Colors.blue, Colors.yellow, Colors.green][index]))
+        ]));
+      });
+
+      return GestureDetector(
+        onTap: () {
+          if (animationController.status == AnimationStatus.dismissed ||
+              animationController.status == AnimationStatus.forward) {
+            animationController.forward();
+          } else {
+            animationController.reverse();
+          }
+        },
+        child: Column(
+          children: list,
+        ),
+      );
+    });
+  }
+
+  Widget build1(BuildContext context) {
+    int count = 3;
+    return LayoutBuilder(
+        builder: (BuildContext context, BoxConstraints constraints) {
+      double width = constraints.biggest.width;
+      double height = constraints.biggest.height / count;
+
+      List<Widget> list = List<Widget>.generate(count, (int index) {
+        double top = index * height;
+
+        // Animation<RelativeRect> anim = RelativeRectTween(
+        //         begin: RelativeRect.fromLTRB(0, 0, 0, 0),
+        //         end: RelativeRect.fromLTRB(0, -top, 0, top - height))
+        //     .animate(animationController);
+        Animation<RelativeRect> anim = animationController
+            .drive(CurveTween(curve: Curves.easeIn))
+            .drive(RelativeRectTween(
+                begin: RelativeRect.fromLTRB(0, 0, 0, 0),
+                end: RelativeRect.fromLTRB(0, -top, 0, top)));
+
+        Animation<Rect> anim1 = animationController
+            .drive(CurveTween(curve: Curves.easeIn))
+            .drive(RectTween(
+                begin: Rect.fromLTWH(0, top, width, height),
+                end: Rect.fromLTWH(index * width, 0, width, height)));
+
+        return RelativePositionedTransition(
+            size: Size(0, 0),
+            rect: anim1,
+            child: Container(
+                width: width,
+                height: height,
+                color: [Colors.blue, Colors.yellow, Colors.green][index]));
+
+        // return Positioned(
+        //   left: 0,
+        //   top: top,
+        //   width: width,
+        //   height: height,
+        //   child: Stack(
+        //     overflow: Overflow.visible,
+        //     children: [
+        //       PositionedTransition(
+        //         rect: anim,
+        //         child: Container(
+        //             color: [Colors.blue, Colors.yellow, Colors.green][index]),
+        //       )
+        //     ],
+        //   ),
+        // );
+      });
+
+      list.add(RaisedButton(
+          onPressed: () {
+            if (animationController.status == AnimationStatus.dismissed ||
+                animationController.status == AnimationStatus.forward) {
+              animationController.forward();
+            } else {
+              animationController.reverse();
+            }
+          },
+          child: Text("animate")));
+
+      return Stack(
+        // overflow: Overflow.visible,
+        children: list,
+      );
+
+      // return Column(
+      //   crossAxisAlignment: CrossAxisAlignment.stretch,
+      //   children: <Widget>[
+      //     Expanded(
+      //         child: Container(
+      //       color: Colors.green,
+      //     )),
+      //     Expanded(
+      //         child: Container(
+      //       color: Colors.yellow,
+      //       child: Stack(children: <Widget>[
+      //         PositionedTransition(
+      //           rect: anim,
+      //           child: Container(
+      //             color: Colors.blue,
+      //           ),
+      //         ),
+      //         RaisedButton(
+      //           onPressed: () {
+      //             animationController.forward();
+      //           },
+      //           child: Text("click"),
+      //         )
+      //       ]),
+      //     )),
+      //   ],
+      // );
+
+      // return Stack(
+      //   children: [
+      //     PositionedTransition(
+      //       rect: anim,
+      //       child: Container(
+      //         color: Colors.blue,
+      //       ),
+      //     ),
+      //     RaisedButton(
+      //       onPressed: () {
+      //         animationController.forward();
+      //       },
+      //       child: Text("click"),
+      //     )
+      //   ],
+      // );
+    });
+
+    // // RelativeRect
+    // return Stack(
+    //   children: <Widget>[
+    //     PositionedTransition(
+    //         rect: anim,
+    //         child: PageView(
+    //           scrollDirection: direction,
+    //           controller: pageController,
+    //           pageSnapping: false,
+    //           children: <Widget>[
+    //             Container(color: Colors.red),
+    //             Container(color: Colors.green),
+    //             Container(color: Colors.blue)
+    //           ],
+    //         )),
+    //     // LayoutBuilder(
+    //     //     builder: (BuildContext context, BoxConstraints constraints) {
+    //     //   return Container(
+    //     //       height: constraints.biggest.height * fraction,
+    //     //       child: PageView(
+    //     //         scrollDirection: direction,
+    //     //         controller: pageController,
+    //     //         pageSnapping: false,
+    //     //         children: <Widget>[
+    //     //           Container(color: Colors.red),
+    //     //           Container(color: Colors.green),
+    //     //           Container(color: Colors.blue)
+    //     //         ],
+    //     //       ));
+    //     // }),
+
+    //     // PageView(
+    //     //   scrollDirection: direction,
+    //     //   controller: pageController,
+    //     //   pageSnapping: false,
+    //     //   children: <Widget>[
+    //     //     Container(color: Colors.red),
+    //     //     Container(color: Colors.green),
+    //     //     Container(color: Colors.blue)
+    //     //   ],
+    //     // ),
+
+    //     // AnimatedPadding(
+    //     //   padding: EdgeInsets.all(val.toDouble()),
+    //     //   duration: Duration(milliseconds: 3000),
+    //     //   child: PageView(
+    //     //     scrollDirection: Axis.horizontal,
+    //     //     children: <Widget>[
+    //     //       Container(color: Colors.red),
+    //     //       Container(color: Colors.green),
+    //     //       Container(color: Colors.blue)
+    //     //     ],
+    //     //   ),
+    //     // ),
+    //     RaisedButton(
+    //         onPressed: () {
+    //           setState(() {
+    //             // val = (val + 8) % 80;
+    //             if (direction == Axis.vertical) {
+    //               direction = Axis.horizontal;
+    //               fraction = 1.0;
+    //             } else {
+    //               direction = Axis.vertical;
+    //               fraction = 0.5;
+    //             }
+    //             pageController = PageController(viewportFraction: fraction);
+    //           });
+    //         },
+    //         child: Text("reload"))
+    //   ],
+    // );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  final String title;
+
+  HomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter;
+  List<String> data = List<String>.generate(100, (int index) {
+    return "Line Line Line Line Line Line Line Line Line Line 99999 9 9 9 99 9 9 9 9 9 9 9 9 9 9  9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 9 99 9 9 9 9 9 99 9 9 9 9 9 9 9: ${index + 1}";
+  });
+
+  @override
+  void initState() {
+    super.initState();
+    _counter = 2;
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
+        // Here we take the value from the HomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          child: ListView.builder(
+              itemCount: data.length,
+              itemBuilder: (BuildContext context, int index) {
+                if (index == 0) {
+                  return LayoutBuilder(builder:
+                      (BuildContext context, BoxConstraints constraints) {
+                    return Container(
+                        height: constraints.biggest.width,
+                        child: PageView(
+                          scrollDirection: Axis.horizontal,
+                          children: <Widget>[
+                            Container(color: Colors.red),
+                            Container(color: Colors.green),
+                            Container(color: Colors.blue)
+                          ],
+                        ));
+                    return Container(
+                        height: constraints.biggest.width,
+                        child: GridView.builder(
+                            padding: const EdgeInsets.all(8.0),
+                            physics: NeverScrollableScrollPhysics(),
+                            gridDelegate:
+                                SliverGridDelegateWithFixedCrossAxisCount(
+                                    mainAxisSpacing: 8.0,
+                                    crossAxisSpacing: 8.0,
+                                    crossAxisCount: _counter),
+                            itemCount: 4,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Container(color: Colors.amber);
+                            }));
+                  });
+                }
+                String text = data[index];
+                return GestureDetector(
+                    onTap: () {
+                      print("tap index: $index");
+                    },
+                    child: Text(index % 2 == 0 ? text : "short"));
+              })),
     );
   }
 }
