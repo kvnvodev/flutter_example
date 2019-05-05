2e0d2852c140a633b00620ec6a62d809da8b86fe
kvnvodev <kvnvo.dev@gmail.com>
2019-05-05 21:46:17 +0700

    Setup spa project

---
+++ flutter_app/lib/pages/home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class SPHomePage extends StatefulWidget {
+  @override
+  _SPHomePageState createState() => _SPHomePageState();
+}
+
+class _SPHomePageState extends State<SPHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Spa"),
+      ),
+      body: Container(),
+    );
+  }
+}
